a4cf2175b8842d46aeb60b7aaeedeb6dca1b906a
Ivan Tabarino <tabarino@outlook.com>
2021-06-04 09:04:26 +0100

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   fisrtElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +12,23 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.fisrtElement = importedNode.firstElementChild as HTMLFormElement;
+    this.fisrtElement.id = 'user-input';
+
+    this.titleInputElement = this.fisrtElement.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.fisrtElement.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.fisrtElement.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.fisrtElement.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
